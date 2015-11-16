f220030dc2115b986c667c8a88f6e33848b8ed36
Michael DeNatale <mikedeeno84@gmail.com>
2015-11-16 18:25:07 -0500

    up through validation on learndot

--- models/index.js
+++ models/index.js
@@ -1,6 +1,31 @@
 var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
 // Notice the `mongodb` protocol; Mongo is basically a kind of server,
 // which handles database requests and sends responses. It's async!
 mongoose.connect('mongodb://localhost/wikistack'); // <= db name will be 'wikistack'
 var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'mongodb connection error:'));
+
+var pageSchema = new Schema({
+	title: {type: String, required: true},
+	urlTitle: {type: String, required: true},
+	content: {type: String, required: true},
+	date: {type : Date. default: Date.now},
+	status: {type: String, enum :['open','closed']},
+	author: {type: Schema.types.Objectid, ref:'User'}
+
+})
+
+var userSchema = new Schema({
+	name: {type: String, required: true},
+	email: {type: String, required: true, unique: true}
+})
+
+var Page = mongoose.model("Page", pageSchema);
+var User = mongoose.model("User", userSchema);
+
+module.exports = {
+	Page: Page,
+	User: User
+};
